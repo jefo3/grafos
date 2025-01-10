2883febc75bcd4b5b1f2ced5647ba9ae3035dc0f
jefo3 <jeferson.goncalves.noronha@gmail.com>
2025-01-10 00:05:07 -0300

    refactor: clean up comments and formatting in DFS implementation

--- dfs.cpp
+++ dfs.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void DFS(int v, vector<vector<int>>& adj, vector<bool>& is_visited) {
   is_visited[v] = true;
   
-  cout << v << " ";  // Exibe o vértice
+  cout << v << " "; 
 
   for (int u: adj[v]) {
     if (!is_visited[u]) {
@@ -19,8 +19,7 @@ void DFS(int v, vector<vector<int>>& adj, vector<bool>& is_visited) {
 int main () {
 
   const int number_of_vertices = 4;
-  
-  //lista de adjacencia
+
   vector<vector<int>> adj{{1, 2}, {0, 2, 3}, {0, 1, 3}, {1, 2}};
   
   vector<bool> is_visited(number_of_vertices, false);
